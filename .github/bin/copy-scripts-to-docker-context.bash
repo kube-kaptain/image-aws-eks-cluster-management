b0ba405af9d9f0188270f0a116bcd3fcd209eb15
Fred Cooke <fred.cooke@gmail.com>
2026-03-01 01:25:13 +1300

    Add age decryption and upgrade to a version of the build scripts that handles tag max parts for initial tags properly doing what the user wants.

--- .github/bin/copy-scripts-to-docker-context.bash
+++ .github/bin/copy-scripts-to-docker-context.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DEPLOY_SCRIPTS_VERSION="1.0.10"
+DEPLOY_SCRIPTS_URL="https://github.com/kube-kaptain/kaptain-deploy-scripts/releases/download/${DEPLOY_SCRIPTS_VERSION}/kaptain-deploy-scripts-${DEPLOY_SCRIPTS_VERSION}.zip"
+
+mkdir -p "${OUTPUT_SUB_PATH}/copy-scripts"
+curl -sLo "${OUTPUT_SUB_PATH}/copy-scripts/deployscripts.zip" "${DEPLOY_SCRIPTS_URL}"
+unzip -o "${OUTPUT_SUB_PATH}/copy-scripts/deployscripts.zip" scripts/decrypt-age -d "${OUTPUT_SUB_PATH}/copy-scripts"
+
 cp src/scripts/* "${DOCKER_CONTEXT_SUB_PATH_LINUX_AMD64}/scripts/"
+cp "${OUTPUT_SUB_PATH}/copy-scripts/scripts/decrypt-age" "${DOCKER_CONTEXT_SUB_PATH_LINUX_AMD64}/scripts/"
+
 cp src/scripts/* "${DOCKER_CONTEXT_SUB_PATH_LINUX_ARM64}/scripts/"
+cp "${OUTPUT_SUB_PATH}/copy-scripts/scripts/decrypt-age" "${DOCKER_CONTEXT_SUB_PATH_LINUX_ARM64}/scripts/"
